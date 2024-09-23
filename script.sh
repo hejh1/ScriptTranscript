28f1145c15b4ed295c058d27f04654efab06d407
hejh1 <hejh1@github.com>
2024-09-24 00:17:35 +0800

    update

--- script.sh
+++ script.sh
@@ -15,17 +15,6 @@ git pull
 cd ../
 cp ./copilot/InterviewCopilot/ViewModels/LocalTranscriptViewModel.swift ./ScriptTranscript/
 
-
-#while [ $# -ge 1 ] ; do
-#        case "$1" in
-#                --audio=*) audioPath=$(echo $1 | awk -F '=' '{print $2}'); shift 1;;
-#                --output=*) outputPath=$(echo $1 | awk -F '=' '{print $2}'); shift 1;;
-#                --a=*) audioPath=$(echo $1 | awk -F '=' '{print $2}'); shift 1;;
-#                --o=*) outputPath=$(echo $1 | awk -F '=' '{print $2}'); shift 1;;
-#                *) echo "unknown parameter $1." ; exit 1 ; break;;
-#        esac
-#done
-
 xcodebuild -scheme ScriptTranscript -configuration Debug -derivedDataPath ./Build
 audioFiles=$(ls ./Resources/audio/ | grep '.wav')
 for file in $audioFiles
@@ -35,8 +24,4 @@ do
   echo $txt
   ./Build/Build/Products/Debug/ScriptTranscript $modelPath $audioPath$file $outputPath$txt
 done
-
-#xcodebuild -scheme ScriptTranscript -configuration Debug -derivedDataPath ./Build
-
-#./Build/Build/Products/Debug/ScriptTranscript $modelPath $audioPath $outputPath
 
